8bef8ee7c8308f0c761c176235215ec24d55debd
Harshit Shukla <harshitshukla1290@gmail.com>
2024-05-30 19:05:40 +0530

    Done the Basic Setup of the App

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+const MONGO_URL = "mongodb://localhost:27017/TravelNest";
+
+const mongoose = require("mongoose");
+
+mongoose.connect(MONGO_URL)
+.then(()=>console.log("MONGODB Connected"))
+.catch((err) => console.log("Error in Connecting to MONGODB",err));
+
+app.get("/",(req,res) => {
+    res.send("Hi from Root");
+});
+
+app.listen(PORT,() => console.log(`Server Started at PORT:${PORT}`));
